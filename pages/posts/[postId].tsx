7b4e0f2ea5753cbeca63a9721f37bb7a4eed585b
binhdao19 <binhchan0293@gmail.com>
2022-02-14 18:28:33 +0700

    SSG getStaticPaths getStaticProps

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,7 +1,10 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/router';
 import * as React from 'react';
 
-export interface PostDetailPageProps {}
+export interface PostDetailPageProps {
+  post: any;
+}
 
 export default function PostDetailPage(props: PostDetailPageProps) {
   const route = useRouter();
@@ -9,7 +12,33 @@ export default function PostDetailPage(props: PostDetailPageProps) {
   return (
     <div>
       <h1>Post Detail Pages</h1>
-      {JSON.stringify(route.query)}
+      <p>{props.post.id}</p>
+      <p>{props.post.title}</p>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  return {
+    paths: data.map((d: any) => ({ params: { postId: `${d.id}` } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  const postId = context.params?.postId;
+
+  if (!postId) return { notFound: true };
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,39 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import Link from 'next/link';
 import * as React from 'react';
 
-export interface PostPageProps {}
+export interface PostPageProps {
+  posts: any[];
+}
 
 export default function PostPage(props: PostPageProps) {
-  return <div>Post pages</div>;
+  return (
+    <div>
+      <h1>Post pages</h1>
+      <ul>
+        {props.posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps<PostPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  console.log(data);
+
+  return {
+    props: {
+      posts: data.map((d: any) => ({ id: d.id, title: d.title })),
+    },
+  };
+};
